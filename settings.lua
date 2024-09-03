ddbbecba8f512405b7081ca3581b6027aa3763db
uwufetch <165817486+uwufetch@users.noreply.github.com>
2024-09-03 20:27:36 +0100

    CleanUp

--- settings.lua
+++ settings.lua
@@ -1,4 +1,6 @@
-return {
-    ['BotToken'] = "BOT_TOKEN",
-    ['Prefix'] = "$"
-}
+local Settings = {}
+
+Settings.BotToken = 'BotToken' -- Discord Bot Token, be cautious, revealing this will grant other users to control your bot!
+Settings.Prefix = '$' -- Discord Bot's prefix, avoid using characters that are characters used for formatting in Lua.
+
+return Settings
